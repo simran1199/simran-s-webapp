0b35e8d85f7d6552619c9921bec9dd0417ab7579
simran <simran3579singh@gmail.com>
2020-05-29 19:31:23 +0530

    initial commit

---
+++ router.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('./controllers/userController')
+const postController=require('./controllers/postController')
+
+
+//user related routes
+router.get('/', userController.home)
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
+
+
+//profile related routes
+router.get("/profile/:username",userController.ifUserExists,userController.profilePostsScreen)
+
+
+//post related routes
+router.get("/create-post",userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post("/create-post", userController.mustBeLoggedIn, postController.create)
+router.get("/post/:id",postController.viewSingle)
+
+module.exports = router
